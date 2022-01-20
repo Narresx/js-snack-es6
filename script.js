58c159baeeb4c9bd8e2b458af375e54671c8f9b1
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-20 17:12:25 +0100

    fix object and first snack

--- script.js
+++ script.js
@@ -8,43 +8,43 @@ Stampare in console la bici con peso minore utilizzando destructuring e template
 */
 
 const bikes = [
-    MERIDA = {
+    {
         nome: 'MERIDA',
         peso: 12,
     },
-    TREK = {
+    {
         nome: 'TREK',
         peso: 11,
     },
-    SPECIALIZED = {
+    {
         nome: 'SPECIALIZED',
         peso: 10,
     },
-    CANNONDALE = {
+    {
         nome: 'CANNONDALE',
         peso: 13,
     },
-    KONA = {
+    {
         nome: 'KONA',
         peso: 14,
     },
-    SCOTT = {
+    {
         nome: 'SCOTT',
         peso: 15,
     },
-    SANTA = {
+    {
         nome: 'SANTA CRUZ',
         peso: 9,
     },
-    MARIN = {
+    {
         nome: 'MARIN',
         peso: 8,
     },
-    GT = {
+    {
         nome: 'GT',
         peso: 7,
     },
-    GIANT = {
+    {
         nome: 'GIANT',
         peso: 6,
     },
@@ -61,15 +61,10 @@ for (let i = 0; i < bikes.length; i++) { // ! Controllo nell'array di oggetti
 
     bikesWeight.push(peso); // aggiungo elementi all'array
 
-    const theLightest = Math.min(...bikesWeight);
-
-    console.log(theLightest); // indica il peso minore
-
-    console.log(bikesWeight);
-
-    if (peso === theLightest) {  // stampo l'oggetto con minore peso tra quelli controllati
-        console.log(bikes[i]);
-    }
 }
 
+console.log(bikesWeight);
+
+const theLightest = Math.min(...bikesWeight);
+console.log(theLightest); // indica il peso minore
 //var m = Math.min(...myArray); funzione che indica il valore minore di un array
--- script1.js
+++ script1.js
@@ -8,3 +8,47 @@ Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+const footballTeams = [    // array di squadre
+    {
+        name: 'Internazionale',
+        points: 0,
+        irregularitiesSuffered: 0,
+    },
+    {
+        name: 'Juventus',
+        points: 0,
+        irregularitiesSuffered: 0,
+    },
+    {
+        name: 'Milan',
+        points: 0,
+        irregularitiesSuffered: 0,
+    },
+    {
+        name: 'Roma',
+        points: 0,
+        irregularitiesSuffered: 0,
+    },
+];
+
+console.log(footballTeams);
+
+const randomNumber = Math.floor(Math.random() * 15) + 1;  // Generatore di numeri random da 1 a 15
+
+const clonedTeams = [];  // array vuoto dove andrò a pushare i nuovi oggetti (oggetto con nome e falli subiti);
+
+let clonedPoints;
+let clonedIrregularitiesSuffered;
+
+for (let i = 0; i < footballTeams.length; i++) {
+    const team = footballTeams[i];
+
+    const { points, irregularitiesSuffered, } = team; // Recupero i valori che mi servono 
+
+
+
+    clonedTeams.push(points, irregularitiesSuffered);
+
+    console.log(clonedTeams);
+}
